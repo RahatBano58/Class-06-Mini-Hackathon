600b481ecc7396e3d038517f46702bc00955d6eb
Rahat Bano <160172836+RahatBano58@users.noreply.github.com>
2024-10-27 20:19:25 +0500

    Add files via upload

---
+++ src/app/Components/CarsCategories.tsx
@@ -0,0 +1,71 @@
+export default function CarsCategories () {
+    return (
+        <div>
+            <div className="flex justify-evenly text-gray-400 bg-gray-800 font-serif font-light  py-10 cursor-pointer">
+                <ul>
+                    <span className="text-white font-semibold text-md">Cars By Make</span>
+                    <li>Toyota Cars for Sale</li>
+                    <li>Suzuki Cars for Sale</li>
+                    <li>Honda Cars for Sale</li>
+                    <li>Daihatsu Cars for Sale</li>
+                    <li>Mitsubishi Cars for Sale</li>
+                    <li>Nissan Cars for Sale</li>
+                    <li>Mercedes Cars for Sale</li>
+                    <li>Hyundai Cars for Sale</li>
+                    <li>BMW Cars for Sale</li>
+                            
+                </ul>
+                
+                <ul>
+                    <span className="text-white font-semibold text-md">Cars By City</span>
+                    <li>Cars in Lahore</li>
+                    <li>Cars in Karachi</li>
+                    <li>Cars in Islamabad</li>
+                    <li>Cars in Rawalpindi</li>
+                    <li>Cars in Peshawar</li>
+                    <li>Cars in Faisalabad</li>
+                    <li>Cars in Multan</li>
+                    <li>Cars in Gujranwala</li>
+                    <li>Cars in Sialkot</li>
+                    <li>Cars in Sargodha</li>
+                </ul>
+                   
+                <ul>
+                    <span className="text-white font-semibold text-md">Explore PakWheels</span>
+                    <li>Used Cars</li>             
+                    <li>Used Bikes</li>             
+                    <li>New Cars</li>      
+                    <li>Auto Parts & Accessories</li>
+                    <li>Cool Rides</li>  
+                    <li>Forums</li>
+                    <li>Autoshow</li>
+                    <li>Sitemap</li>
+                </ul>
+
+                <ul>
+                    <span className="text-white font-semibold text-md">PakWheels.com</span>
+                        <li>About PakWheels.com</li>
+                        <li>Our Products</li>
+                        <li>Advertise With Us</li>
+                        <li>How To Pay</li>
+                        <li>FAQs</li>
+                        <li>Refunds & Returns</li>
+                        <li>Careers</li>
+                        <li>Contact Us</li>
+                </ul>
+
+                <ul>
+                    <span className="text-white font-semibold text-md">Cars by Province</span>
+                    <li>Cars in Punjab</li>
+                    <li>Cars in Sindh</li>
+                    <li>Cars in KPK</li>
+                    <li>Cars in Balochistan</li>
+                    <li>Cars in Azad Kashmir</li>
+                    <li>Cars in Federally Administered</li>
+                    <li>Tribal Area</li>
+                </ul>
+            </div>
+            <hr/>
+        </div>
+    )
+}
---
+++ src/app/Thankyou/page.tsx
@@ -0,0 +1,9 @@
+export default function ThankYou(){
+    return(
+        <div className="flex flex-col justify-center items-center 
+         px-12 md:p-44 text-black text-center bg-sky-100 h-screen">
+                <h1 className="text-blue-500 font-bold italic text-3xl md:text-7xl ">Thank you for shopping!!!</h1>
+                <p className="pt-12">Your order will be delivered in 3 Business days </p>
+        </div>
+    );
+}
