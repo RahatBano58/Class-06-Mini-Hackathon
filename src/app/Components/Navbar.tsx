665966dbde22c2845dfbbcbc9c064295001b1dc4
Rahat Bano <160172836+RahatBano58@users.noreply.github.com>
2024-10-27 20:58:43 +0500

    Update Navbar.tsx

--- src/app/Components/Navbar.tsx
+++ src/app/Components/Navbar.tsx
@@ -14,16 +14,16 @@ export default function Navbar() {
             </div>
             </Link>
             <div className="flex justify-center gap-12 items-end py-4 px-8 ">
-               <Link href="/" className="text-white text-md font-semibold font-sans w- h-8 hover:text-red-500 cursor-pointer">Used Cars▼</Link>
-               <Link href="/" className="text-white text-md font-semibold font-sans w- h-8 hover:text-red-500 cursor-pointer">New Cars▼</Link>
-               <Link href="/" className="text-white text-md font-semibold font-sans w- h-8 hover:text-red-500 cursor-pointer">Bikes▼</Link>
-               <Link href="/" className="text-white text-md font-semibold font-sans w- h-8 hover:text-red-500 cursor-pointer">Auto Store▼</Link>
-               <Link href="/" className="text-white text-md font-semibold font-sans w- h-8 hover:text-red-500 cursor-pointer">Videos▼</Link> 
-               <Link href="/" className="text-white text-md font-semibold font-sans w- h-8 hover:text-red-500 cursor-pointer">Forums▼</Link>
-               <Link href="/" className="text-white text-md font-semibold font-sans w- h-8 hover:text-red-500 cursor-pointer">Blogs▼</Link>
-               <Link href="/" className="text-white text-md font-semibold font-sans w- h-8 hover:text-red-500 cursor-pointer">More▼</Link>
+               <Link href="/" className="text-white text-md font-semibold font-sans w- h-8 hover:text-red-500 cursor-pointer">Used Cars⏷</Link>
+               <Link href="/" className="text-white text-md font-semibold font-sans w- h-8 hover:text-red-500 cursor-pointer">New Cars⏷</Link>
+               <Link href="/" className="text-white text-md font-semibold font-sans w- h-8 hover:text-red-500 cursor-pointer">Bikes⏷</Link>
+               <Link href="/" className="text-white text-md font-semibold font-sans w- h-8 hover:text-red-500 cursor-pointer">Auto Store⏷</Link>
+               <Link href="/" className="text-white text-md font-semibold font-sans w- h-8 hover:text-red-500 cursor-pointer">Videos⏷</Link> 
+               <Link href="/" className="text-white text-md font-semibold font-sans w- h-8 hover:text-red-500 cursor-pointer">Forums⏷</Link>
+               <Link href="/" className="text-white text-md font-semibold font-sans w- h-8 hover:text-red-500 cursor-pointer">Blogs⏷</Link>
+               <Link href="/" className="text-white text-md font-semibold font-sans w- h-8 hover:text-red-500 cursor-pointer">More⏷</Link>
                 <div className="bg-red-500 text-white py-2 px-8 text-lg font-semibold font-sans rounded-lg hover:bg-red-700 cursor-pointer">
-                     <button >Post an Ad▼</button>
+                     <button >Post an Ad⏷</button>
                 </div>
             </div>
         </nav>
