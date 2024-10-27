2b1d44983e3cf7717d5b862ceed0106255f23608
Rahat Bano <160172836+RahatBano58@users.noreply.github.com>
2024-10-27 21:23:22 +0500

    Update Footer.tsx

--- src/app/Components/Footer.tsx
+++ src/app/Components/Footer.tsx
@@ -7,7 +7,7 @@ export default function Footer (){
             <br/>
             <div className="text-center font-bold text-white text-xl font-sans py-4 hover:text-blue-600 cursor-pointer  ">
                <Link href="https://www.linkedin.com/in/rahat-bano-5b78b41b3/" >Developed By: Rahat Bano</Link>
-               <p className=" text-center text-xl font-semibold text-white hover:text-yellow-300 cursor-pointer">HomeWork Done! Given By Sir <b>"Ali Jawwad"</b> Slot: Tuesday 7:00 PM - 10:00 PM</p>
+               <p className=" text-center text-xl font-semibold text-white hover:text-yellow-300 cursor-pointer">HomeWork Done! Given By Sir <b>&quot;Ali Jawwad&quot;</b> Slot: Tuesday 7:00 PM - 10:00 PM</p>
             </div>
         </div>
             
